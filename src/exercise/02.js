08186ec37066d5e3c10ec18f7e980e0aa4123781
Andre Roussakoff <madrus@gmail.com>
2023-04-27 18:21:38 +0200

    kcd's version of extra 1 (part 2)

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -4,28 +4,16 @@
 import * as React from 'react'
 import {Switch} from '../switch'
 
-
 function Toggle(props) {
-	const [on, setOn] = React.useState(false)
+  const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
 
-  // 🐨 replace this with a call to React.Children.map and map each child in
-  // props.children to a clone of that child with the props they need using
-  // React.cloneElement.
-  // 💰 React.Children.map(props.children, child => {/* return child clone here */})
-  return React.Children.map(props.children, child => {
-    /* return child clone here */
-		if (typeof child.type === 'string') {
-			return child
-		}
-    const newChild = React.cloneElement(child, {on, toggle})
-    return newChild
-  })
-  // 📜 https://reactjs.org/docs/react-api.html#reactchildren
-  // 📜 https://reactjs.org/docs/react-api.html#cloneelement
+  return React.Children.map(props.children, child =>
+    allowedTypes.includes(child.type)
+      ? React.cloneElement(child, {on, toggle})
+      : child
+  )
 }
-
-// 🐨 Flesh out each of these components
 
 // Accepts `on` and `children` props and returns `children` if `on` is true
 const ToggleOn = ({on, children}) => (on ? children : null)
@@ -36,8 +24,13 @@ const ToggleOff = ({on, children}) => (on ? null : children)
 // Accepts `on` and `toggle` props and returns the <Switch /> with those props.
 const ToggleButton = ({on, toggle}) => <Switch on={on} onClick={toggle} />
 
+//=> this list will cause our MyToggleMessage component to be ignored
+const allowedTypes = [ToggleOn, ToggleOff, ToggleButton]
+//=> this list will allow our MyToggleMessage to render correctly
+// const allowedTypes = [ToggleOn, ToggleOff, ToggleButton, MyToggleMessage]
+
 function MyToggleMessage({on, toggle}) {
-	return on ? 'the button is on yo!' : 'the button is off sooooooo...'
+  return on ? 'the button is on yo!' : 'the button is off sooooooo...'
 }
 
 function App() {
@@ -48,7 +41,7 @@ function App() {
         <ToggleOff>The button is off</ToggleOff>
         <span>Hello</span>
         <ToggleButton />
-				<MyToggleMessage />
+        <MyToggleMessage />
       </Toggle>
     </div>
   )
